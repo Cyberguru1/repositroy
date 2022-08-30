a836fb5e43cac400ce6790e64fe3a0496bf69163
hamza <hamzasaidu34@gmail.com>
2022-08-29 21:18:36 -0400

    adjusted parser

--- main.c
+++ main.c
@@ -12,8 +12,14 @@ void main(void){
     out = get_line(&buff, &var, stdin);
     token = parse_cmd(buff); // sotring the output into an array
 
-    for (int i = 0; token[i] != NULL; i++) //looping and printing each element
+   /* for (int i = 0; token[i] != NULL; i++) //looping and printing each element
     {
         printf("%s\n", token[i]);
+    } */
+
+    if (execve(token[0], token, NULL) == -1)
+    {
+        perror("Error:");
     }
+    
 }
