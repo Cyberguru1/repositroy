01a7f7e48aa88fa2260cb5ab205cfc5112dc3e18
hamza <hamzasaidu34@gmail.com>
2022-08-29 20:56:05 -0400

    added parser

---
+++ array_cmd.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * parse_cmd - parse line of input
+ * @input: input parameter form getline
+ * Return: an array of the input
+ */
+
+char **parse_cmd(char *input)
+{
+    char **tokens, *token;
+    int i;
+    char delim[2] = " "
+
+    i = 0;
+
+    if (input == NULL)
+       return NULL;
+    if ((*tokens = malloc(sizeof(char) * BUFFSIZE)) == NULL)
+       return NULL;
+
+    token = _strtok(input, delim);
+
+    while (token != NULL)
+    {
+        tokens[i] = token;
+        token = _strtok(NULL, delim);
+        i++;
+    }
+
+    return tokens;
+}
