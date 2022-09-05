d6fca019ad525efbfa9c03a7a22ee794a6ec0e48
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-05 19:06:01 -0400

    added more buitin functions

--- input_check.c
+++ input_check.c
@@ -19,4 +19,28 @@ int repeated_char(char *input, int i)
  * @last: last char read
  * Return: index of char
  */
-int error
+int error_sep_op(char *input, int i, char last)
+{
+    int count;
+
+    count = 0;
+    if (*input == '\0')
+      return (0);
+    
+    if (*input == ' ' || * input == '\t')
+       return (error_sep_op(input + 1, i + 1, last));
+
+    if (*input == ';')
+      if (last == '|' || last == '&' || last == ';')
+        return (i);
+    if (*input == '|')
+    {
+         if (last == ';' || last == '&') return (i);
+         if (last == '|')
+         {
+             count = repeated_char(input, 0);
+             if (count == 0 || count > 1) return (i);
+         }
+    }
+    
+}
