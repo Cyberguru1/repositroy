abb54a0210fd75762d1923a1a138391859a46e69
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-05 19:00:01 -0400

    added more buitin functions

--- input_check.c
+++ input_check.c
@@ -1,3 +1,13 @@
 #include "main.h"
 /**
  * repeated_char - counts the repetitions of character in a string
+ * @input: inputed string
+ * @i: index of char to check at
+ * Return: number of repetitions
+ */
+int repeated_char(char *input, int i)
+{
+    if(*(input - i) == *input)
+       return (repeated_char(input - 1, i + 1));
+    return (i);
+}
