9c1bbb7aaebdd4d7dc5ab83d7b3b78491b2ecaa0
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-07 03:22:01 -0400

    added more buitin functions

--- list_ops.c
+++ list_ops.c
@@ -87,29 +87,6 @@ line_list *add_line_node_end(line_list **head, char *line)
 
 	return (*head);
 }
-
-/**
- * free_line_list - frees a line_list
- * @head: head of the linked list.
- * Return: no return.
- */
-void free_line_list(line_list **head)
-{
-	line_list *temp;
-	line_list *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
 /**
  * add_rvar_node - adds a variable at the end
  * of a r_var list.
@@ -146,26 +123,4 @@ r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
 	}
 
 	return (*head);
-}
-
-/**
- * free_rvar_list - frees a r_var list
- * @head: head of the linked list.
- * Return: no return.
- */
-void free_rvar_list(r_var **head)
-{
-	r_var *temp;
-	r_var *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
 }
---
+++ list_ops2.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * free_rvar_list - frees a r_var list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+void free_rvar_list(r_var **head)
+{
+	r_var *temp;
+	r_var *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+/**
+ * free_line_list - frees a line_list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+void free_line_list(line_list **head)
+{
+	line_list *temp;
+	line_list *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
