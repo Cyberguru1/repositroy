b84d3ee814630a6a8b258c975af40a24362fcce5
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-07 00:14:01 -0400

    added more buitin functions

--- main.c
+++ main.c
@@ -1,26 +1,8 @@
 #include "main.h"
 
 int main(void){
-    size_t var;
-    char *buff = NULL;
-    ssize_t out;
-    char **token;
-
-    
-    var = 0;
-    printf("$ ");
+  
     out = get_line(&buff, &var, stdin);
     token = parse_cmd(buff); 
-   /* for (int i = 0; token[i] != NULL; i++)
-   {
-        printf("%s\n", token[i]);
-    } */
-    printf("%lu", out);
-    if (execve(token[0], token, NULL) == -1)
-    {
-        perror("Error:");
-        return (0);
-        
-    }
-    return (0);
+ 
 }
