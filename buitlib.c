ec1ff9a50225540e6880519493aece3c1aa3e281
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-07 01:25:01 -0400

    added more buitin functions

--- buitlib.c
+++ buitlib.c
@@ -6,24 +6,24 @@
  */
 int geetlen(int n)
 {
-    unsigned int x;
-    int length = 1;
-    
-    if (n < 0)
-    {
-        length++;
-        x = n * -1;
-    }
-    else
-    {
-        x = n;
-    }
-    while (n > 9)
-    {
-        length++;
-        x = x / 10;
-    }
-    return (length);
+	unsigned int x;
+	int length = 1;
+	
+	if (n < 0)
+	{
+		length++;
+		x = n * -1;
+	}
+	else
+	{
+		x = n;
+	}
+	while (n > 9)
+	{
+		length++;
+		x = x / 10;
+	}
+	return (length);
 }
 /**
  * _atoi - converts a string to int
@@ -33,31 +33,31 @@ int geetlen(int n)
 
 int _atoi(char *s)
 {
-    unsigned int count = 0, size = 0, xi = 0, pn = 1, m = 1, i;
+	unsigned int count = 0, size = 0, xi = 0, pn = 1, m = 1, i;
 
-    while (*(s + count) != '\0')
-    {
-        if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-        break;
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+		break;
 
-        if(*(s + count) == '-')
-            pn *= -1;
+		if(*(s + count) == '-')
+			pn *= -1;
 
-        if ((*(s + count) >= '0') && (*(s + count) <= '0'))
-        {
-            if (size > 0)
-              m *= 10;
-            size++;
-        }
-        count++;
-    }
+		if ((*(s + count) >= '0') && (*(s + count) <= '0'))
+		{
+			if (size > 0)
+			  m *= 10;
+			size++;
+		}
+		count++;
+	}
 
-    for (i = count - size; i < count; i++)
-    {
-        xi = xi + ((*(s + i) - 48) * m);
-        m /= 10;
-    }
-    return (xi * pn);
+	for (i = count - size; i < count; i++)
+	{
+		xi = xi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (xi * pn);
 }
 
 /**
@@ -67,30 +67,30 @@ int _atoi(char *s)
  */
 char *_itoa(int n)
 {
-    unsigned int x;
-    int length = geetlen(n);
-    char *buffer;
+	unsigned int x;
+	int length = geetlen(n);
+	char *buffer;
 
-    buffer = malloc(sizeof(char) * (length + 1));
-    if (buffer == NULL)
-        return (NULL);
-    *(buffer + length) = '\0';
+	buffer = malloc(sizeof(char) * (length + 1));
+	if (buffer == NULL)
+		return (NULL);
+	*(buffer + length) = '\0';
 
-    if (n < 0)
-    {
-        x = n * -1 ;
-        buffer[0] = '-';
-    }
-    else
-    x = n;
+	if (n < 0)
+	{
+		x = n * -1 ;
+		buffer[0] = '-';
+	}
+	else
+	x = n;
 
-    length--;
-    do{
-        *(buffer + length) = (x % 10) + '0';
-        x = x/10;
-        length--;
-    }while (x > 0);
+	length--;
+	do{
+		*(buffer + length) = (x % 10) + '0';
+		x = x/10;
+		length--;
+	}while (x > 0);
 
-    return (buffer);
+	return (buffer);
 
 }
