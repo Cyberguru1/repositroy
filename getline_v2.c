000376dd63ddbef43a7d59f4b8b9d9c3c944b7c4
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-07 02:12:36 -0700

    adjusted getline

--- getline_v2.c
+++ getline_v2.c
@@ -7,7 +7,8 @@
  * Return: size of bytes on sucess or -1 on failure
  */
 
-ssize_t get_line(char **buff, size_t *buffsize, FILE *fp){
+ssize_t get_line(char **buff, size_t *buffsize, FILE *fp)
+{
 	char *ptr, *eptr, *nbuff;
 	ssize_t nbuffsize, d;
 	int c;
@@ -17,7 +18,7 @@ ssize_t get_line(char **buff, size_t *buffsize, FILE *fp){
 	{
 		*buffsize = BUFFSIZE;
 		if ((*buff = malloc(*buffsize)) == NULL)
-		   return (-1);
+		return (-1);
 	}
 	for (ptr = *buff, eptr = *buff + *buffsize;;)
 	{
@@ -25,26 +26,28 @@ ssize_t get_line(char **buff, size_t *buffsize, FILE *fp){
 		if (c == -1)
 		{
 			if (feof(fp))
-			   return ptr == *buff ? -1 : ptr - *buff;
+			return ptr == *buff ? -1 : ptr - *buff;
 
 			else 
-			   return -1;
-		} 
-		*ptr++ = c;
-		if (c == '\n'){
-			*ptr = '\0';
-			return ptr - *buff;
+			return (-1);
 		}
-		if (ptr + 2 >= eptr){
+		*ptr++ = c;
+		if (c == '\n')
+		{
+			*ptr = '\0';
+			return (ptr - *buff);
+		}
+		if (ptr + 2 >= eptr)
+		{
 			nbuffsize = *buffsize * 2;
 			d = ptr - *buff;
 			if ((nbuff = realloc(*buff, nbuffsize)) == NULL)
-				return -1;
+				return (-1);
 			*buff = nbuff;
 			*buffsize = nbuffsize;
 			eptr = nbuff + nbuffsize;
 			ptr = nbuff + d;
-	
 		}
 	}
 }
+
