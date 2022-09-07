26dde3f60f3db9c3f539fffbea296a76e293e9a1
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-07 05:19:01 -0400

    added more buitin functions

--- getline_v2.c
+++ getline_v2.c
@@ -1,49 +1,86 @@
 #include "main.h"
-/**
- * get_line - program that returns input
- * @buff: buffer to store input
- * @buffsize: size of buffer
- * @fp: file descrptor
- * Return: size of bytes on sucess or -1 on failure
- */
-ssize_t get_line(char **buff, size_t *buffsize, FILE *fp)
-{   char *ptr, *eptr, *nbuff;
-	ssize_t nbuffsize, d;
-	int c;
 
-	if (*buff == NULL || *buffsize == 0)
-	{	*buffsize = BUFFSIZE;
-		*buff = malloc(*buffsize);
-		if (*buff == NULL)
-		return (-1);
-	}
-	for (ptr = *buff, eptr = *buff + *buffsize;;)
+/**
+ * bring_line - assigns the line var for get_line
+ * @lineptr: Buffer that store the input str
+ * @buffer: str that is been called to line
+ * @n: size of line
+ * @j: size of buffer
+ */
+void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
+{
+
+	if (*lineptr == NULL)
 	{
-		c = fgetc(fp);
-		if (c == -1)
-		{
-			if (feof(fp))
-			return (ptr == *buff ? -1 : ptr - *buff);
-			else
-			return (-1);
-		}
-		*ptr++ = c;
-		if (c == '\n')
-		{	*ptr = '\0';
-			return (ptr - *buff);
-		}
-		if (ptr + 2 >= eptr)
-		{   nbuffsize = *buffsize * 2;
-			d = ptr - *buff;
-			nbuff = realloc(*buff, nbuffsize);
-			if (nbuff == NULL)
-			return (-1);
-			*buff = nbuff;
-			*buffsize = nbuffsize;
-			eptr = nbuff + nbuffsize;
-			ptr = nbuff + d;
-		}
+		if  (j > BUFFSIZE)
+			*n = j;
+
+		else
+			*n = BUFFSIZE;
+		*lineptr = buffer;
 	}
+	else if (*n < j)
+	{
+		if (j > BUFFSIZE)
+			*n = j;
+		else
+			*n = BUFFSIZE;
+		*lineptr = buffer;
+	}
+	else
+	{
+		_strcpy(*lineptr, buffer);
+		free(buffer);
+	}
+}
+/**
+ * get_line - Read inpt from stream
+ * @lineptr: buffer that stores the input
+ * @n: size of lineptr
+ * @stream: stream to read from
+ * Return: The number of bytes
+ */
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
+{
+	int i;
+	static ssize_t input;
+	ssize_t retval;
+	char *buffer;
+	char t = 'z';
+
+	if (input == 0)
+		fflush(stream);
+	else
+		return (-1);
+	input = 0;
+
+	buffer = malloc(sizeof(char) * BUFFSIZE);
+	if (buffer == 0)
+		return (-1);
+	while (t != '\n')
+	{
+		i = read(STDIN_FILENO, &t, 1);
+		if (i == -1 || (i == 0 && input == 0))
+		{
+			free(buffer);
+			return (-1);
+		}
+		if (i == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+		if (input >= BUFFSIZE)
+			buffer = _realloc(buffer, input, input + 1);
+		buffer[input] = t;
+		input++;
+	}
+	buffer[input] = '\0';
+	bring_line(lineptr, n, buffer, input);
+	retval = input;
+	if (i != 0)
+		input = 0;
+	return (retval);
 }
 /**
  * read_line - reads the input string.
