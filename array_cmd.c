d600824818d48b3f170f64fc837ade92e414bdd5
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-07 01:06:01 -0400

    added more buitin functions

--- array_cmd.c
+++ array_cmd.c
@@ -8,28 +8,28 @@
 
 char **parse_cmd(char *input)
 {   char **tokens, *token;
-    int i;
-    char delim[5] = "\n ";
+	int i;
+	char delim[5] = "\n ";
 
-    i = 0;
+	i = 0;
 
-    if (input == NULL){
-       return NULL;
-    }
-    tokens = malloc(sizeof(char) * BUFFSIZE);
-    if (tokens == NULL)
-    {  perror("hsh");
-       return NULL;
-    }
+	if (input == NULL){
+	   return NULL;
+	}
+	tokens = malloc(sizeof(char) * BUFFSIZE);
+	if (tokens == NULL)
+	{  perror("hsh");
+	   return NULL;
+	}
 
-    token = _strtok(input, delim);
+	token = _strtok(input, delim);
 
-    while (token != NULL)
-    {
-        tokens[i] = token;
-        token = _strtok(NULL, delim);
-        i++;
-    }
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		token = _strtok(NULL, delim);
+		i++;
+	}
 
-    return tokens;
+	return tokens;
 }
