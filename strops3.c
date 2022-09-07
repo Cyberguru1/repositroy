61004a59e9939d351a47b4b6dd8be1d44e0deb31
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-07 02:38:01 -0400

    added more buitin functions

--- strops3.c
+++ strops3.c
@@ -1,4 +1,52 @@
 #include "main.h"
+
+/**
+ * _strncmp - compares N characters of string
+ * check 'man strncmp' for more detail
+ * @s1: first string
+ * @s2: second string
+ * @n: size of bytes for comparison
+ * Return: returns 1 on success and 0 on failure
+ */
+int _strncmp(char *s1, char *s2, size_t n)
+{
+	size_t i;
+	if (s1 == NULL)
+	   return (-1);
+	for (i = 0; i < n && s2[i]; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * _strdup - Duplicate a string
+ * @s1: string1
+ * Return: returns string duplicate
+ */
+ char *_strdup(char *s1)
+ {
+	 int i;
+	 int x;
+	 char *s2;
+
+	 i = _strlen(s1);
+
+	 if((s2 = malloc(sizeof(char) * (i + 1))) == NULL)
+		 return NULL;
+
+	 for (x = 0; x < i; x++)
+	 {
+		 s1[x] = s2[x];
+	 }
+	 s2[x] = '\0';
+
+	return s2;
+}
 /**
  * cmpChar - comapres each char in a string
  * @str1: array of string
